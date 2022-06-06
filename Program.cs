59691dfdac6305224ae7f7f0e8812363d9da94a7
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-06 21:23:04 +0530

    [Tushar Vidwans] Adding welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FrequencyOfWords_Hashtable_Day15
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the program where we are counting words in a string");
+        }
+    }
+}
