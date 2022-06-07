0f4cb51b9ecb78a41b239edc7963574bb6013ff2
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-08 04:13:46 +0530

    [Tushar Vidwans] Counting the frequency of the words in a smaller string

---
+++ Frequency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyOfWords_Hashtable_Day15
+{
+    public class Frequency
+    {
+        Hashtable MyMapNode = new Hashtable();
+        private String[] sentenceSplit;
+
+
+        public string[] splitString(string sentence)
+        {
+            
+             return sentence.Split();
+            
+        }
+
+
+        public void countWords(string[] sentenceSplit)
+        {
+            foreach (String s in sentenceSplit)
+            {
+                if (MyMapNode.ContainsKey(s))
+                {
+                    MyMapNode[s]= (int)MyMapNode[s] + 1;
+                }
+                else
+                {
+                    MyMapNode.Add(s, 1);
+                }
+            }
+            foreach (DictionaryEntry item in MyMapNode)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+        }
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the program where we are counting words in a string");
+
+            Frequency frequency = new Frequency();
+            string str = "to be or not to be";
+            string[] sentencesplit = frequency.splitString(str);
+            frequency.countWords(sentencesplit);
+
         }
     }
 }
